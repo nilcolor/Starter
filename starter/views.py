0537d5c029d58683f050d0fa5e624e3b703a4b7f
Alexey Blinov <nilcolor@gmail.com>
2011-10-12 19:52:48 +0400

    Default view for request methods which didn't have dedicated view callable

--- starter/views.py
+++ starter/views.py
@@ -10,9 +10,12 @@ class FuckyView(object):
         self.request = request
 
     @view_config(context=Bar)
+    def default(self):
+        return Response('default view called...')
+
+    @view_config(context=Bar, request_method='GET')
     def fucky(self):
-        print self.request
-        return Response('fucky view')
+        return Response('GET:fucky view')
 
 class FuckyViewSecond(object):
     def __init__(self, request):
@@ -20,7 +23,7 @@ class FuckyViewSecond(object):
 
     @view_config(context="starter.resources.Bar", request_method='POST')
     def fucky(self):
-        return Response('fucky view second', )
+        return Response('POST:fucky view second', status="201")
 
 def my_view(request):
     return {'project':'Starter 0.0'}
