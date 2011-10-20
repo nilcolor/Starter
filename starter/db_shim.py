f21ab93b6f93fcf63389b8e09c40c12437ce205c
Alexey Blinov <nilcolor@gmail.com>
2011-10-20 14:40:16 +0400

    DB shim
    real DB... I'll think about it but for now - shim is OK

---
+++ starter/db_shim.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+class DBShim:
+    def __init__(self):
+        self.customer = [
+            {
+                "id": 1,
+                "name": "First",
+                "gender": "m",
+            },
+            {
+                "id": 2,
+                "name": "Second",
+                "gender": "m",
+            },
+            {
+                "id": 3,
+                "name": "Third",
+                "gender": "f",
+            },
+            {
+                "id": 4,
+                "name": "Fourth",
+                "gender": "m",
+            },
+        ]
+        self.order = [
+            {
+                "id": 11,
+                "client": 1,
+                "number": "ORD000011",
+            },
+            {
+                "id": 12,
+                "client": 3,
+                "number": "ORD000012",
+            },
+            {
+                "id": 13,
+                "client": 3,
+                "number": "ORD000013",
+            },
+        ]
+
+class OrderModel(object):
+    """docstring for Order"""
+
+    @staticmethod
+    def init_with_id(id):
+        db = DBShim()
+        return OrderModel(db.order[id])
+
+    def __init__(self, data):
+        super(OrderModel, self).__init__()
+        if data:
+            self.id = data["id"]
+            self.client = data["client"]
+            self.number = data["number"]
+
+    def all(self):
+        return DBShim().order
+
+    def one(self, num):
+        for x in DBShim().order:
+            if x["id"] == num:
+                return x
+        return {}
+
+
+class Customer(object):
+    """docstring for Customer"""
+    def __init__(self, data):
+        super(Customer, self).__init__()
+        self.id = data["id"]
+        self.name = data["name"]
+        self.gender = data["gender"]
